088981973c3547e332416de02fe3619ae5aa3932
JinXin <741799837@qq.com>
2024-05-25 11:37:15 +0800

    初始化塔的子弹基类 创建子弹的蓝图类 设置子弹的材质

---
+++ Source/StoneDefence/Private/Bullet/RuleOfTheBullet.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bullet/RuleOfTheBullet.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+
+// Sets default values
+ARuleOfTheBullet::ARuleOfTheBullet()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootBullet = CreateDefaultSubobject<USceneComponent>(TEXT("BulletRootBullet"));
+	BoxDamage = CreateDefaultSubobject<USphereComponent>(TEXT("BulletNoxDamage"));
+	ProjectileMoement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("BulletProjectileMovement"));
+
+	RootComponent = RootBullet;
+	BoxDamage->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	
+	ProjectileMoement->MaxSpeed = 2000.f;//最大移动速度
+	ProjectileMoement->InitialSpeed = 1600.0f;//初始移动速度
+	ProjectileMoement->ProjectileGravityScale = 0.0f;//重力为0，不会有掉落效果
+	ProjectileMoement->UpdatedComponent = BoxDamage;
+}
+
+// Called when the game starts or when spawned
+void ARuleOfTheBullet::BeginPlay()
+{
+	Super::BeginPlay();
+	//绑定代理
+	BoxDamage->OnComponentBeginOverlap.AddUniqueDynamic(this, &ARuleOfTheBullet::OnBeginOverlap);
+}
+
+// Called every frame
+void ARuleOfTheBullet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ARuleOfTheBullet::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
+
---
+++ Source/StoneDefence/Public/Bullet/RuleOfTheBullet.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RuleOfTheBullet.generated.h"
+
+UCLASS()
+class STONEDEFENCE_API ARuleOfTheBullet : public AActor
+{
+	GENERATED_BODY()
+	//根组件
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseAttrubute", meta = (AllowPrivateAccess = "true"))
+	class USceneComponent* RootBullet;
+	//碰撞盒子
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseAttrubute", meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* BoxDamage;
+	//具有移动属性的组件
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseAttrubute", meta = (AllowPrivateAccess = "true"))
+	class UProjectileMovementComponent* ProjectileMoement;
+
+public:	
+	// Sets default values for this actor's properties
+	ARuleOfTheBullet();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
